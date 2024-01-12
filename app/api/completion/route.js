840d97138f04985b068966333b78737c541c0245
Ranahansa <CODSE213F-053@student.nibm.lk>
2024-01-12 15:07:22 +0530

    commit 2

---
+++ app/api/completion/route.js
@@ -0,0 +1,37 @@
+import {StreamingTextResponse, CohereStream} from 'ai'
+
+export const runtime = 'edge'
+
+export async function POST(req) {
+    const {prompt} = req;
+
+    const body = JSON.stringify({
+        prompt,
+        model: 'command-nightly',
+        max_tokens: 300,
+        stop_sequences: ['\n'],
+        temperature: .9,
+        return_likelihoods: 'NONE',
+        stream: true
+
+    } )
+
+    const response =  await fetch('https://api.cohere.ai/v2/generate', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${process.env.COHERE_API_KEY}`,
+            'Content-Type': 'application/json'
+        },
+        body
+    })
+
+    if (!response.ok) {
+        return new Response(await response.text(), {
+            status:response.status
+        })
+    }
+    const stream = CohereStream(response)
+
+    return new StreamingTextResponse(stream)
+    
+}
--- app/page.js
+++ app/page.js
@@ -3,9 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      
     </div>
   )
 }
